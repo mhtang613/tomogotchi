bbefed12d655602340d1320e20a78d5645b86f2e
MaxTang6 <mhtang@andrew.cmu.edu>
2023-12-01 14:15:23 -0500

    add null check to mood bar

--- tomogotchi/static/messages.js
+++ tomogotchi/static/messages.js
@@ -66,7 +66,9 @@ class MessageHandler {
             }
         })
         let mood_meter = document.getElementById("mood");
-        mood_meter.value = response.mood;
+        if (!mood_meter) {
+            mood_meter.value = response.mood;
+        }
     }
 
     // Builds a new HTML "div" element for each message
